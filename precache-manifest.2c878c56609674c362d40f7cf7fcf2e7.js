b4f4b73a5b780a635eb9b2ce89af2c717792fec6
Veniamin Bezborodov <VeniaminBezborodov@yandex.ru>
2020-08-08 15:00:36 +0500

    Updates

---
+++ precache-manifest.2c878c56609674c362d40f7cf7fcf2e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231152228a2f0b35cb425c6fb10a7d59",
+    "url": "/Messenger/index.html"
+  },
+  {
+    "revision": "dedec551512f26b1b83e",
+    "url": "/Messenger/static/css/main.959cb87d.chunk.css"
+  },
+  {
+    "revision": "f14fd6e18312e2608130",
+    "url": "/Messenger/static/js/2.06e68f95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Messenger/static/js/2.06e68f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dedec551512f26b1b83e",
+    "url": "/Messenger/static/js/main.93b5c54d.chunk.js"
+  },
+  {
+    "revision": "a48ee220ad1d70ade9b6",
+    "url": "/Messenger/static/js/runtime-main.90fea234.js"
+  }
+]);
